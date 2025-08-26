9e33192bd5e8124ed4ce78268ca413ebe740a5ce
isadora.rodrigues@ucsal.edu.br <Isalyratvd@gmail.com>
2025-08-25 23:45:14 -0300

    Refatoração da class ReceiptService (Calcula subtotal, aplica imposto usando o TaxCalculator e gera o recibo usando o ReceiptFormatter)

--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -1,5 +1,16 @@
 package br.com.mariojp.solid.srp;
 
 public class TaxCalculator {
+	private double taxRate;
 
+	public TaxCalculator() {
+
+		// Recupera a taxa de imposto da configuração, padrão para o 0.10
+		String rate = System.getProperty("tax.rate", "0.10");
+		this.taxRate = Double.parseDouble(rate);
+	}
+
+	public double calculateTax(double subtotal) {
+		return subtotal * taxRate;
+	}
 }
